e2b008853170a54dce4d17f40da69ae9163b23bd
GitByRen <1402443141@qq.com>
2018-08-21 20:19:28 +0800

    little

--- src/main/java/com/important/crud/handlers/EmployeeHandler.java
+++ src/main/java/com/important/crud/handlers/EmployeeHandler.java
@@ -1,7 +1,30 @@
 package com.important.crud.handlers;
 
+import java.util.Collection;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.important.crud.dao.EmployeeDao;
+
+@Controller
 public class EmployeeHandler {
 
-	
-	
+    @Autowired
+    private EmployeeDao employeeDao;
+
+    @RequestMapping("/input")
+    public String input() {
+        
+        return "input";
+    }
+
+    @RequestMapping("/emps")
+    public String emps(Map<String, Object> map) {
+        map.put("employees", employeeDao.getAll());
+        return "list";
+    }
+
 }
